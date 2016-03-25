c6cc18236626f47ee8100f863767d40a03c77779
Ilya Mozerov <imozerov@ccc-team-mac1.local>
2016-03-25 16:29:09 +0300

    Added bad implemntation of UiCollectionView for Favourites

--- StreetArtView/FavouritesViewController.swift
+++ StreetArtView/FavouritesViewController.swift
@@ -8,16 +8,47 @@
 
 import UIKit
 
-class FavouritesViewController: UICollectionViewController {
+class FavouritesViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
+    var data: Array<ArtObjectUi>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        data = DataSource.instance.listArtObjects()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data!.count
+    }
+    
+    //3
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        cell.backgroundColor = UIColor.blackColor()
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                               sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
+    
+    func collectionView(collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                               insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return sectionInsets
     }
     
     
