aaead682dae0ad0f28094105f9cb1367c044acda
Ilya Mozerov <imozerov@ccc-team-mac1.local>
2016-03-29 17:39:20 +0300

    added dummy detail view

---
+++ StreetArtView/ArtObjectDetailController.swift
@@ -0,0 +1,19 @@
+//
+//  ArtObjectDetailController.swift
+//  StreetArtView
+//
+//  Created by Ilya Mozerov on 3/29/16.
+//  Copyright © 2016 street art view. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArtObjectDetailController : UIViewController {
+    
+    @IBOutlet weak var address: UILabel!
+    @IBOutlet weak var artObjectDescription: UILabel!
+    @IBOutlet weak var author: UILabel!
+    @IBOutlet weak var artObjectTitle: UILabel!
+    @IBOutlet weak var image: UIImageView!
+}
